30f3ab75fc04b152ad4bc9d3df072168e94d35f7
Orel Balilti <orelba93@gmail.com>
2021-09-27 23:44:28 +0300

    initial commit

---
+++ apps/robust-micro-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'robust-micro-frontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'robust-micro-frontend';
+}
